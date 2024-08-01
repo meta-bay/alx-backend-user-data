4c26d5c0cd4e2bf809acc60758c60e31a22c58dc
meta-bay <bayiemeseret@gmail.com>
2024-08-01 23:17:42 +0300

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -22,8 +22,6 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    logging.basicConfig(level=logging.INFO, format=FORMAT)
-
     def __init__(self, fields):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
