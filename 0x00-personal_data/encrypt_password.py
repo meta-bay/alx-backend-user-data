33bd00e4b80fd9f0a9f8ddae64301c4246349f4b
meta-bay <bayiemeseret@gmail.com>
2024-08-02 04:56:20 +0300

    Check valid password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+encrypt_password
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ Returns a salted, hashed password, which is a byte string """
+    encoded = password.encode()
+    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Validates if the provided password matches the hashed password """
+    valid = False
+    encoded = password.encode()
+    if bcrypt.checkpw(encoded, hashed_password):
+        valid = True
+    return valid
