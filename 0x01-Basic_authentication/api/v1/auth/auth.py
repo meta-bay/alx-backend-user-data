29b4bca57e0f7ef16b64981c5f947281495ae3de
meta-bay <bayiemeseret@gmail.com>
2024-08-06 15:41:04 +0300

    Define which routes dont

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,16 @@ class Auth():
     """ authentication class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         ''' require authentication '''
-        return False
+        if not path or not excluded_paths:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        for excluded_path in excluded_paths:
+            if not excluded_path.endswith('/'):
+                excluded_path += '/'
+            if path == excluded_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization header """
