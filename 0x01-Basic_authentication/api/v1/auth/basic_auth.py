d0f410d50492cae5c8c5e1ad04a7fb359ac4d8c4
meta-bay <bayiemeseret@gmail.com>
2024-08-07 04:51:28 +0300

    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,9 @@
 """ basic auth """
 from api.v1.auth.auth import Auth
 import base64
+from models.base import Base
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -45,3 +48,35 @@ class BasicAuth(Auth):
             return (None, None)
         email, password = decoded_base64_authorization_header.split(':')
         return (email, password)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ returns the User instance based on his email and password """
+        if user_email is None or type(user_email) != str:
+            return None
+        if user_pwd is None or type(user_pwd) != str:
+            return None
+        users = User.search({'email': user_email})
+        if users:
+            if users and users != []:
+                for user in users:
+                    if user.is_valid_password(user_pwd):
+                        return user
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ overloads Auth and retrieves the User instance for a request """
+        auth_header = self.authorization_header(request)
+        if not auth_header:
+            return None
+        encoded = self.extract_base64_authorization_header(auth_header)
+        if not encoded:
+            return None
+        decoded = self.decode_base64_authorization_header(encoded)
+        if not decoded:
+            return None
+        email, pwd = self.extract_user_credentials(decoded)
+        if not email or not pwd:
+            return None
+        user = self.user_object_from_credentials(email, pwd)
+        return user
