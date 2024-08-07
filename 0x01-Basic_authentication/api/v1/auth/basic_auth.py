0baa981961ce7e89bf07bdffb1376fd149f4fecd
meta-bay <bayiemeseret@gmail.com>
2024-08-07 05:23:34 +0300

    Basic - Allow password with ":"

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -46,7 +46,7 @@ class BasicAuth(Auth):
             return (None, None)
         if ':' not in decoded_base64_authorization_header:
             return (None, None)
-        email, password = decoded_base64_authorization_header.split(':')
+        email, password = decoded_base64_authorization_header.split(':', 1)
         return (email, password)
 
     def user_object_from_credentials(
