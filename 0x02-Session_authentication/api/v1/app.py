205444ef536c78a9e81403293a64c46ae6c547a6
meta-bay <bayiemeseret@gmail.com>
2024-08-08 19:16:50 +0300

    fix code

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,14 +36,14 @@ def filtering():
                 '/api/v1/unauthorized/',
                 '/api/v1/forbidden/',
                 '/api/v1/auth_session/login/']
-    if (auth.authorization_header(request) and auth.session_cookie(request)):
-        return None, abort(401)
     if auth.require_auth(request.path, ex_paths):
         if auth.authorization_header(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
         request.current_user = auth.current_user(request)
+    if (auth.authorization_header(request) and auth.session_cookie(request)):
+        return None, abort(401)
 
 
 @app.errorhandler(404)
