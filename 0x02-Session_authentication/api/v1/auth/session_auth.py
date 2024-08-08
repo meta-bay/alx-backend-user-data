31d5391640a8aeef693a809e2a4edcc17af9722e
meta-bay <bayiemeseret@gmail.com>
2024-08-08 18:46:55 +0300

    fix code

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,7 +12,7 @@ class SessionAuth(Auth):
         """ creates a Session ID for a user_id """
         if user_id is None or not isinstance(user_id, str):
             return None
-        session_id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
@@ -20,4 +20,5 @@ class SessionAuth(Auth):
         """ returns a User ID based on a Session ID """
         if session_id is None or not isinstance(session_id, str):
             return None
-        return self.user_id_by_session_id.get(session_id)
+        user_id = self.user_id_by_session_id.get(session_id)
+        return user_id
