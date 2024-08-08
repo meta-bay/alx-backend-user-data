866b79c5c133adab2702028894046e87a178dc6b
meta-bay <bayiemeseret@gmail.com>
2024-08-08 19:02:06 +0300

    Session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """ authentication """
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth():
@@ -42,3 +43,10 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """ current user """
         return None
+
+    def session_cookie(self, request=None):
+        """ returns a cookie value from a request """
+        if request is None:
+            return None
+        _my_session_id = os.getenv('SESSION_NAME')
+        return request.cookies.get(_my_session_id)
