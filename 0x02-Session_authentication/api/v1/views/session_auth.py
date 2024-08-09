e84467baaffde06918a2bb548d3d52e362ffeea2
meta-bay <bayiemeseret@gmail.com>
2024-08-09 04:46:45 +0300

    Logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -35,3 +35,14 @@ def auth_session_login():
     response = jsonify(user.to_json())
     response.set_cookie(SESSION_NAME, session_id)
     return response
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
+def auth_session_logout():
+    """ DELETE /api/v1/auth_session/logout """
+    from api.v1.app import auth
+    deleted = auth.destroy_session(request)
+    if not deleted:
+        abort(404)
+    return jsonify({}), 200
