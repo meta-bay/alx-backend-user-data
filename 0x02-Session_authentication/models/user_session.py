2bfe411c629a71825b59f9b2cc7c687b5436eba3
meta-bay <bayiemeseret@gmail.com>
2024-08-09 05:59:10 +0300

    done

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" User session
+"""
+from models.base import Base
+
+
+class UserSession(Base):
+    """User Session Class
+    """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """initialize"""
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
