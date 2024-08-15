cc71dd489994a8fab7a09ce7327e439c9d4864af
meta-bay <bayiemeseret@gmail.com>
2024-08-15 12:38:52 +0300

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ auth module """
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> str:
@@ -8,3 +10,21 @@ def _hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
     hashed_passwd = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_passwd
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ user Registeration """
+        try:
+            self._db.find_user_by(email=email)
+        except Exception:
+            raise ValueError(f"User {email} already exists")
+        hashed_password = _hash_password(password)
+        new_user = self._db.add_user(email, hashed_password)
+        return new_user
