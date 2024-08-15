69be211f2f688ca06099ceff26ed3b2caea2cb93
meta-bay <bayiemeseret@gmail.com>
2024-08-15 16:00:39 +0300

    Register user through api

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python3
 """ flask app module """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import requests
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
 def message():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def users():
+    """ register user 'POST /users' """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify(
+            {"email": email, "message": "user created"}
+        )
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
