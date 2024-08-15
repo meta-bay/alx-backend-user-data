22487c7c3dfbba96adb44556afcaa31a3abb916e
meta-bay <bayiemeseret@gmail.com>
2024-08-15 17:51:42 +0300

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -33,7 +33,7 @@ class Auth:
         new_user = self._db.add_user(email, hashed_password)
         return new_user
 
-    def valid_login(self, email, password) -> bool:
+    def valid_login(self, email: str, password: str) -> bool:
         """ credentials validation """
         try:
             user = self._db.find_user_by(email=email)
