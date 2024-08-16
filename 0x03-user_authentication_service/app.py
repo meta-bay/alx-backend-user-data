4f69e1a4313af13aa1af8338a12fc98f2b4bde7c
meta-bay <bayiemeseret@gmail.com>
2024-08-16 03:27:50 +0300

    log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -45,7 +45,7 @@ def login():
 @app.route("/sessions", methods=['DELETE'], strict_slashes=False)
 def logout():
     """ respond to the DELETE /sessions route """
-    session_id = request.form.get('session_id')
+    session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
