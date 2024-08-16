28506bf3469990a8f65765e0ee96d4bebae1d1ca
meta-bay <bayiemeseret@gmail.com>
2024-08-16 03:02:34 +0300

    Destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -82,11 +82,4 @@ class Auth:
         """ Destroy session """
         if user_id is None:
             return None
-        try:
-            user = self._db.find_user_by(user_id=user_id)
-            if user:
-                setattr(user, user.session_id, None)
-                self._db._session.commit()
-            return None
-        except NoResultFound:
-            return None
+        self._db.update_user(user_id, None)
