7a24d7a71d95b6505ee220e25849c2ae40d8c7e4
meta-bay <bayiemeseret@gmail.com>
2024-08-16 04:22:43 +0300

    Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -77,13 +77,13 @@ def get_reset_password_token():
 @app.route("/reset_password", methods=['PUT'], strict_slashes=False)
 def update_password():
     """ respond to the PUT /reset_password route """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
     try:
-        email = request.form.get('email')
-        reset_token = request.form.get('reset_token')
-        new_password = request.form.get('new_password')
-        AUTH.update_password(reset_token, new_password)
+        AUTH.update_password(reset_token, password=new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
-    except (ValueError, KeyError):
+    except ValueError:
         abort(403)
 
 
