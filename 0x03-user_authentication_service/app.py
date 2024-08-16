2569a49e4ce6b5c8c3e785648172cc38ede06335
meta-bay <bayiemeseret@gmail.com>
2024-08-16 04:17:08 +0300

    Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -74,7 +74,7 @@ def get_reset_password_token():
         abort(403)
 
 
-@app.route("/reset_password", methods=['POST'], strict_slashes=False)
+@app.route("/reset_password", methods=['PUT'], strict_slashes=False)
 def update_password():
     """ respond to the PUT /reset_password route """
     email = request.form.get('email')
